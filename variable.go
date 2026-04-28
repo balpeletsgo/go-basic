ce14fd68ccdff407046a4797e2494cf7ff0317bb
Iqbal Pamula <balpamula@gmail.com>
2026-04-28 23:27:55 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Iqbal Pamula"
+	fmt.Println(name)
+
+	name = "Balpe Geming"
+	fmt.Println(name)
+
+	var (
+		firstName = "Iqbal"
+		lastName  = "Pamula"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
