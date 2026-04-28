088b53bc06289637ef3bc8244b5040bd9256a76a
Iqbal Pamula <balpamula@gmail.com>
2026-04-28 23:15:12 +0700

    string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Iqbal")
+	fmt.Println("Iqbal Pamula")
+	fmt.Println("Iqbal Pamula Ba'iq")
+
+	fmt.Println(len("Iqbal"))
+	fmt.Println("Iqbal Pamula"[0])
+	fmt.Println("Iqbal Pamula Ba'iq"[7])
+}
